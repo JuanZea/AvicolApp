bad212420f0f0b2e0b6cc25afe73799d46430b64
Juan David Zea <vikzea@gmail.com>
2021-08-22 11:08:33 -0500

    fix: Services imports

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 import settlementsService from "./settlementsService"
-import {barnsService} from "./barnsService";
-import {lotsService} from "./lotsService";
+import barnsService from "./barnsService";
+import lotsService from "./lotsService";
 
 export {settlementsService, lotsService, barnsService}
