3f1f1a5677903a55b57de1716dd9a2ac9b289d4f
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2021-09-22 00:11:09 -0500

    feat: add edit form

--- src/components/tables/LotsTable.vue
+++ src/components/tables/LotsTable.vue
@@ -1,45 +1,46 @@
 <template>
   <div class="flex flex-col justify-between h-full">
-    <div>
-      <delete-lot-modal v-if="lotActive" :open="openDeleteLotModal" :lot="lotActive" @close="openDeleteLotModal = false" @deleted="refresh" />
-      <av-table :headers="['Numero de lote', 'Edad (Semanas)', 'Numero de Gallinas', 'Fecha de creación', '']">
-        <tr v-for="(lot, key) in lotsByPage" :key="key" class="border-b border-gray-200 hover:bg-gray-100">
-          <td class="py-3 px-6 text-center whitespace-nowrap">
-              <span class="font-bold">{{ lot.id }}</span>
-          </td>
-          <td class="py-3 px-6 text-center">
-            {{ lot.age }}
-          </td>
-          <td class="py-3 px-6 text-center">
+    <delete-lot-modal v-if="lotActive" :open="openDeleteLotModal" :lot="lotActive" @close="openDeleteLotModal = false"
+                      @deleted="refresh"/>
+    <av-table :headers="['Numero de lote', 'Edad (Semanas)', 'Numero de Gallinas', 'Fecha de creación', '']">
+      <tr v-for="(lot, key) in lotsByPage" :key="key" class="border-b border-gray-200 hover:bg-gray-100">
+        <td class="py-3 px-6 text-center whitespace-nowrap">
+          <span class="font-bold">{{ lot.id }}</span>
+        </td>
+        <td class="py-3 px-6 text-center">
+          {{ lot.age }}
+        </td>
+        <td class="py-3 px-6 text-center">
               <span class="rounded-lg text-gray-600 bg-gray-300 px-2 py-1 font-bold bg-opacity-50">
                 {{ lot.hens_number }}
               </span>
-          </td>
-          <td class="py-3 px-6 text-center">
-              {{convertDate(lot.created_at)}}
-          </td>
-          <td class="py-3 px-6 text-center">
-            <div class="flex item-center justify-center gap-2">
-              <button @click="router.push({name: 'showLots', params: {id: lot.id}})" class="w-4 mr-2 transform hover:scale-125 cursor-pointer">
-                <fai class="text-av-100 hover:text-av-300" size="lg" icon="eye"/>
-              </button>
-              <button type="button" @click="openDeleteModal(lot)"  class="w-4 mr-2 transform hover:scale-110 cursor-pointer">
-                <fai class="text-av-100 hover:text-av-300" size="lg" icon="trash-alt"/>
-              </button>
-            </div>
-          </td>
-        </tr>
-      </av-table>
-    </div>
+        </td>
+        <td class="py-3 px-6 text-center">
+          {{ convertDate(lot.created_at) }}
+        </td>
+        <td class="py-3 px-6 text-center">
+          <div class="flex item-center justify-center gap-2">
+            <button @click="router.push({name: 'showLots', params: {id: lot.id}})"
+                    class="w-4 mr-2 transform hover:scale-125 cursor-pointer">
+              <fai class="text-av-100 hover:text-av-300" size="lg" icon="eye"/>
+            </button>
+            <button type="button" @click="openDeleteModal(lot)"
+                    class="w-4 mr-2 transform hover:scale-110 cursor-pointer">
+              <fai class="text-av-100 hover:text-av-300" size="lg" icon="trash-alt"/>
+            </button>
+          </div>
+        </td>
+      </tr>
+    </av-table>
     <paginator v-if="lots" :items="lots" :page-size="10" @changePage="onChangePage"></paginator>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import { useSettlements } from "../../use";
+import {ref} from "vue";
+import {useSettlements} from "../../use";
 import dayjs from "dayjs";
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import lowerCase from "lodash/lowerCase";
 import capitalize from "lodash/capitalize";
 import "dayjs/locale/es"
@@ -49,9 +50,9 @@ import DeleteLotModal from "../modals/DeleteLotModal.vue";
 
 export default {
 
-  components: { DeleteLotModal, Paginator, AvTable },
+  components: {DeleteLotModal, Paginator, AvTable},
 
-  props: { lots: {required: true, type: Array} },
+  props: {lots: {required: true, type: Array}},
 
   setup(props, computed) {
 
@@ -59,7 +60,7 @@ export default {
     let lotActive = ref();
     const router = useRouter();
     let lotsByPage = ref([]);
-    const { activeSettlement } = useSettlements();
+    const {activeSettlement} = useSettlements();
 
     const convertDate = (date) => dayjs(date).locale('es').format('MMMM D, h:mm a');
 
--- src/views/auth/barns/ShowBarns.vue
+++ src/views/auth/barns/ShowBarns.vue
@@ -12,7 +12,14 @@
           <div><span class="font-glory text-2xl text-gray-500">{{ capitalize(lowerCase(barn.type)) }}</span></div>
         </div>
 
-        <av-input v-else v-model="newName" id="name" type="text" label="Nuevo nombre"/>
+        <div v-else class="flex flex-col lg:flex-row gap-3 w-96 lg:w-full px-4">
+          <av-input v-model="newName" id="name" type="text" label="Nuevo nombre:"/>
+          <av-select v-model="newType" id="type" class="w-full" label="Tipo:">
+            <option class="bg-av-50 bg-opacity-10" value="cautividad">Cautividad</option>
+            <option class="bg-av-50 bg-opacity-10" value="gallinasFelices">Gallinas Felices</option>
+          </av-select>
+        </div>
+
         <div v-if="!editMode" class="flex self-center gap-2 lg:ml-0">
           <button @click="editMode = true"
                   class="group items-center flex bg-black p-2 bg-opacity-5 rounded-full bg-opacity-5 hover:bg-opacity-10 lg:rounded lg:py-1">
@@ -21,18 +28,30 @@
           </button>
         </div>
         <div class="flex flex-col self-start gap-2 ml-4 lg:ml-0 lg:flex-row" v-else>
-          <button @click="updateUser"
+          <button @click="updateBarn"
                   class="bg-opacity-10 self-start flex bg-black p-1 bg-opacity-5 rounded-full">
             <fai class="text-av-100" :icon="['far', 'check-circle']" size="lg"/>
           </button>
-          <button @click="editMode = false; newName = name"
+          <button @click="editMode = false; newName = barn.name"
                   class="bg-opacity-10 self-start flex bg-black p-1 bg-opacity-5 rounded-full">
             <fai class="text-av-100" :icon="['far', 'times-circle']" size="lg"/>
           </button>
         </div>
       </div>
     </div>
-    <lots-table :lots="lots"></lots-table>
+    <lots-table v-if="lots && lots.length" :lots="lots"></lots-table>
+    <div class="w-full m-2 md:m-6 flex flex-grow flex-col md:flex-row justify-center items-center gap-6" v-else>
+      <img class="object-contain max-h-96" src="/src/assets/illustrations/not_found.svg">
+      <div class="flex flex-col">
+        <h1 class="text-3xl">No se han encontrado lotes para este galpón.</h1>
+        <div class="my-3">
+          <h2>Puedes crear uno aquí...</h2>
+          <button type="button" class="btn btn-persimmon text-white font-glory my-3">
+            Crear lote
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -44,16 +63,20 @@ import {useRouter} from "vue-router";
 import {barnsService, lotsService} from "../../../services";
 import LotsTable from "../../../components/tables/LotsTable.vue";
 import {_updateBarn} from "../../../services/avicolappAssembler";
+import AvInput from "../../../components/forms/AvInput.vue";
+import AvSelect from "../../../components/forms/AvSelect.vue";
 
 export default {
-  components: {LotsTable},
-  setup() {
-    const editMode = ref(false);
 
+  components: {AvInput, AvSelect, LotsTable},
+  setup() {
+
+    const editMode = ref(false);
     const router = useRouter();
     const barn = ref();
     const id = computed(() => router.currentRoute.value.params.id);
     const newName = ref();
+    const newType = ref();
     const lots = ref({});
     _updateBarn(id.value);
 
@@ -61,13 +84,25 @@ export default {
         .then(response => {
           lots.value = response;
         })
+
     barnsService.one(id.value)
         .then(response => {
           barn.value = response;
           newName.name = barn.value.name;
+          newType.value = barn.value.type;
         })
 
-    return {barn, router, capitalize, lowerCase, editMode, newName, lots}
+    const updateBarn = () => {
+      barnsService.update(id.value, {
+        'name': newName.value,
+        'type': newType.value,
+      }).then(response => {
+        editMode.value = false
+        barn.value = response;
+      })
+    }
+
+    return {barn, router, capitalize, lowerCase, editMode, newName, newType, updateBarn, lots}
 
   }
 
